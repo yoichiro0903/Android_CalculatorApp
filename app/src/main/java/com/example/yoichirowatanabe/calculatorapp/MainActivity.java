f6b771dacca03db4f3667cc56207fdd96a69aec1
Yoichiro Watanabe <yoichiro.watanabe@o-07975-mac.local>
2017-07-10 10:01:30 +0900

    customized navigation drawer.

--- app/src/main/java/com/example/yoichirowatanabe/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/yoichirowatanabe/calculatorapp/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.yoichirowatanabe.calculatorapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -30,28 +28,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
 
         getRadioButtonChecked();
+        makeDrawerLayout();
+
+    }
+
+    //Create Navigation Drawer
+    public void makeDrawerLayout (){
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
     }
+
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -65,6 +60,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
 
+
+    //Calculate method
     public void numButtonClick (View v){
         Button numButton = (Button) v;
         inputNum = inputNum + numButton.getText().toString();
@@ -92,9 +89,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         }
     }
-
-
-
 
     public void showInputNumToLayout (String inputNumString) {
         TextView inputNumShowTV = (TextView) findViewById(R.id.inputTextView);
